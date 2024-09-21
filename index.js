e0dc68f445dc86960caf03572d890c8eb1b740ce
David Neves <davidoneves563@gmail.com>
2024-09-21 15:21:31 -0300

    ADD ROTA PARA LANDING PAGE

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
-
-const alertasRoutes = require('./routes/alertas');
+const alertasRoutes = require('./alertas');
 
 app.use(express.json());
 
-app.use('/alertas', alertasRoutes);
+app.get('/landing', (req, res) => {
+    res.sendFile(path.join(__dirname, 'landing.html'));
+});
 
 app.get('/', (req, res) => {
     res.send('Bem-vindo ao sistema de alerta de crises de Belo Horizonte');
 });
 
-app.listen(port, () => {
-    console.log(`API de alertas de crises rodando em http://localhost:${port}`);
+app.use('/alertas', alertasRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
