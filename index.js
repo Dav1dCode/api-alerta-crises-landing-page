f65b4559eab092fcb1679de811355b694a13e37d
David Neves <davidoneves563@gmail.com>
2024-09-21 15:40:13 -0300

    CORREÇÕES NO INDEX

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require('express');
+const cors = require('cors'); 
 const path = require('path');
 const app = express();
-const alertasRoutes = require('./alertas');
+const alertasRoutes = require('./routes/alertas'); 
 
-app.use(cors());
-app.use(express.json());
+app.use(cors()); 
+app.use(express.json()); 
 
-app.use(express.static(path.join(__dirname)));
-
+app.use(express.static(path.join(__dirname))); 
 
 app.get('/landing', (req, res) => {
-    res.sendFile(path.join(__dirname, 'landing.html'));
+    res.sendFile(path.join(__dirname, 'landing.html')); 
 });
 
 app.get('/', (req, res) => {
     res.send('Bem-vindo ao sistema de alerta de crises de Belo Horizonte');
 });
 
-app.use('/alertas', alertasRoutes);
+app.use('/alertas', alertasRoutes); 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`); 
 });
