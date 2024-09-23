7883f227a59c6a493fb272f9bce6c9fe2e3c520d
David Neves <davidoneves563@gmail.com>
2024-09-23 12:47:10 -0300

    ALTERACOES PARA SERVIR REACT

--- index.js
+++ index.js
@@ -7,19 +7,15 @@ const alertasRoutes = require('./routes/alertas');
 app.use(cors()); 
 app.use(express.json()); 
 
-app.use(express.static(path.join(__dirname))); 
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
-app.get('/landing', (req, res) => {
-    res.sendFile(path.join(__dirname, 'landing.html')); 
+app.use('/alertas', alertasRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
-
-app.get('/', (req, res) => {
-    res.send('Bem-vindo ao sistema de alerta de crises de Belo Horizonte');
-});
-
-app.use('/alertas', alertasRoutes); 
 
 const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`); 
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
